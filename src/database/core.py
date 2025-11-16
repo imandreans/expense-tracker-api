75ffddcd9fcde47830b6291f35988d59881c67bc
imandreans <andrean.sihombing83@gmail.com>
2025-11-16 19:32:15 +0800

    feat: Change the database from PostgreSQL to Supabase

--- src/database/core.py
+++ src/database/core.py
@@ -1,7 +1,7 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os 
-# from supabase import Client, create_client
+from supabase import Client, create_client
 from typing import Annotated
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
@@ -11,26 +11,32 @@ from fastapi import Depends
 dotenv_path = 'src/.env'
 load_dotenv(dotenv_path)
 
-URL_DATABASE = 'postgresql://postgres:12345@localhost:5432/expense'
+# HOST_DB = os.getenv("HOST_DB")
+# NAME_DB = os.getenv("NAME_DB")
+# USER_DB = os.getenv("USER_DB")
+# PASSWORD_DB = os.getenv("PASSWORD_DB")
+# PORT_DB = os.getenv("PORT_DB")
 
-engine = create_engine(URL_DATABASE)
+# URL_DATABASE = f'postgresql://{USER_DB}:{PASSWORD_DB}@{HOST_DB}:{PORT_DB}/expense'
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# engine = create_engine(URL_DATABASE)
 
-Base = declarative_base()
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-def connect_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Base = declarative_base()
 
-DBSession = Annotated[Session, Depends(connect_db)]
 # def connect_db():
-#     SUPABASE_URL = os.getenv("SUPABASE_URL")
-#     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-#     db: Client = create_client(SUPABASE_URL,SUPABASE_KEY)
-#     return db
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-# DbSession = Annotated[Client, Depends(connect_db)]
+# DBSession = Annotated[Session, Depends(connect_db)]
+def connect_db():
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    db: Client = create_client(SUPABASE_URL,SUPABASE_KEY)
+    return db
+
+DBSession = Annotated[Client, Depends(connect_db)]
--- src/expenses/controller.py
+++ src/expenses/controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from . import service
 from ..auth.service import CurrentUser
 from .model import CreateExpense
-from ..database.core import DbSession
+from ..database.core import DBSession
 
 router = APIRouter(
     prefix="/expense",
@@ -10,17 +10,17 @@ router = APIRouter(
 )
 
 @router.post('/add/')
-def add_expense(db: DbSession, current_user: CurrentUser, expense: CreateExpense):
+def add_expense(db: DBSession, current_user: CurrentUser, expense: CreateExpense):
     return service.create_expenses(db, current_user, expense)
 
 @router.delete('/delete/<id>')
-def delete_expense(db: DbSession,current_user: CurrentUser, id):
+def delete_expense(db: DBSession,current_user: CurrentUser, id):
     return service.delete_expense(db, current_user, id)
 
 @router.patch('/edit/<id>')
-def edit_expense(db: DbSession,current_user: CurrentUser, id, expense: CreateExpense):
+def edit_expense(db: DBSession,current_user: CurrentUser, id, expense: CreateExpense):
     return service.edit_expense(db, current_user, id, expense)
 
 @router.get("/")
-def list_expenses(db: DbSession, current_user: CurrentUser):
+def list_expenses(db: DBSession, current_user: CurrentUser):
     return service.list_all_expenses(db, current_user)
